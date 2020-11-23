e10011c93237b1aa723006169e8714439d94e591
alisonhussey <alisonhussey@gmail.com>
2020-11-23 23:17:40 +0000

    A function to return the first element of an array and check it against test cases

--- head.js
+++ head.js
@@ -3,12 +3,12 @@ const assertEqual = function(actual, expected) {
     console.log(`💁‍♀️💁‍♀️💁‍♀️ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🙅‍♀️🙅‍♀️🙅‍♀️ Assertion Failed: ${actual} !== ${expected}`);
-  } 
+  }
 };
 
 const head = function(arr) {
   return arr[0];
-}
+};
 
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
