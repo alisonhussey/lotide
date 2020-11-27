5bd88fa28a31fbca1acd3fc07faeb6120604bdbe
alisonhussey <alisonhussey@gmail.com>
2020-11-27 02:03:34 +0000

    A function which returns a slice of an array up until callback returns truthy value

---
+++ takeUntil.js
@@ -0,0 +1,40 @@
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (callback(item)) {
+      break;
+    }
+    results.push(item);
+  }
+  return results;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`😍😍😍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
