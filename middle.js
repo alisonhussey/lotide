1fb87f88063057de28e1b6a2a49176c68775f2ff
alisonhussey <alisonhussey@gmail.com>
2020-11-30 20:38:37 +0000

    updated middle

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-/*const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(middle(actual), expected)) {
-    console.log(`😍😍😍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-*/
-
 const middle = function(array) {
   if (array.length < 3) {
     return [];
